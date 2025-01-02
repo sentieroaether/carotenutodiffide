3b3433e0228a153fbab39a5e7e1acadebed2cb49
Angelo Guarracino <angeloguarracino@MacBook-Air-di-Angelo.local>
2025-01-02 15:57:38 +0100

    Aggiunti miglioramenti per la conversione DOCX to PDF con LibreOffice

--- docx_to_pdf.py
+++ docx_to_pdf.py
@@ -1,31 +1,39 @@
-import subprocess
 import os
+import subprocess
+
 from utilities import find_executable_by_partial_path
 
 
-libreoffice_path = "libreoffice"
+#search_directory = "C:\\"  # Root directory (use with caution)
+#partial_path = "soffice.exe"
+#libreoffice_path = find_executable_by_partial_path(search_directory, partial_path)
+libreoffice_path = "/opt/homebrew/bin/soffice"
 
 
 def convert_docx_to_pdf(input_path, output_path):
     """
-    Converte un file DOCX in PDF utilizzando LibreOffice.
+    Convert a DOCX file to PDF using LibreOffice.
+
+    Parameters:
+    - input_path: Path to the input DOCX file.
+    - output_path: Path to the output PDF file.
     """
-    # Percorso esplicito di LibreOffice
-    libreoffice_path = "/Applications/LibreOffice.app/Contents/MacOS/soffice"
-    
-    # Comando per la conversione
+    # Define the command to run LibreOffice in headless mode for conversion
     command = [
-        libreoffice_path,
-        "--headless",  # Esegue senza GUI
-        "--convert-to", "pdf",
-        "--outdir", os.path.dirname(output_path),  # Directory di output
-        input_path
+        libreoffice_path, #"C:\\Program Files\\LibreOffice\\program\\soffice.exe",  # LibreOffice executable
+        "--headless",  # Run without GUI
+        "--convert-to", "pdf",  # Conversion format
+        "--outdir", os.path.dirname(output_path),  # Output directory
+        input_path  # Input file
     ]
-    
-    # Esegui il comando
+
     try:
         # Run the command
         subprocess.run(command, check=True)
         print(f"Conversion successful: {output_path}")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred during conversion: {e}")
+
+# Example usage:
+#convert_docx_to_pdf("C:\\Users\\Golden Bit\\Desktop\\projects_in_progress\\angelo_guarracino_projects\\UseCase1\\input_data\\TemplateLetteraDiffida.docx", "C:\\Users\\Golden Bit\\Desktop\\projects_in_progress\\angelo_guarracino_projects\\UseCase1\\output_data\\output.pdf")
+
